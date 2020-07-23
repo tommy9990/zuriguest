753d25ee08410bd0c2654b4004a9f14dfb0aacc5
tommy9990 <68697931+tommy9990@users.noreply.github.com>
2020-07-23 16:17:28 +0200

    Initial commit

---
+++ zuri.js
@@ -0,0 +1,12 @@
+alert("Welcome to Zuri")
+$ class fixed-header {
+  constructor() {
+
+  }
+}("#hide").click(function(){
+  $("p").hide();
+});
+
+$("#show").click(function(){
+  $("p").show();
+});
